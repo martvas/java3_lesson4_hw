b0d914782c925ad6d1e4a8d9107ee0c9fd6b2158
martvas <vasiljevs.martons@gmail.com>
2017-08-27 16:50:03 +0200

    Delete comments

--- src/task1/AbcPrinter.java
+++ src/task1/AbcPrinter.java
@@ -59,43 +59,6 @@ public class AbcPrinter {
             }
         }
     }
-
-//
-//    public void printA() {
-//        synchronized (mon) {
-//            try {
-//                for (int i = 0; i < 3; i++) {
-//                    while (currentLetter != 'A') {
-//                        mon.wait();
-//                    }
-//
-//                    System.out.print("A");
-//                    currentLetter = 'B';
-//                    mon.notifyAll();
-//                }
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    public void printB() {
-//        synchronized (mon) {
-//            try {
-//                for (int i = 0; i < 3; i++) {
-//                    while (currentLetter != 'B') {
-//                        mon.wait();
-//                    }
-//
-//                    System.out.print("B");
-//                    currentLetter = 'A';
-//                    mon.notifyAll();
-//                }
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
 
 
